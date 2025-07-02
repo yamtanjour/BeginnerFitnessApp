8cf3c8714ef930a12038411e0dcc0de44856117c
Yamahn Tanjour <yamtanjour@gmail.com>
2025-07-02 23:11:26 +0200

    Added screens

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ class HomeScreen(Screen):
 class MyWorkoutsScreen(Screen):
     pass
 
+class WorkoutCreationScreen(Screen):
+    pass
+
+class TipsScreen(Screen):
+    pass
+
 class WorkItApp(App):
     def build(self):
         Window.clearcolor = (0.12, 0.12, 0.12, 1)
         Window.size = (360, 640)
         sm = ScreenManager()
-        sm.add_widget(HomeScreen(name='home'))  # <-- use 'home'
+        sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(MyWorkoutsScreen(name='my_workouts'))
+        sm.add_widget(WorkoutCreationScreen(name='workout_creation'))
+        sm.add_widget(TipsScreen(name='tips'))
         return sm
     
 if __name__ == "__main__":
