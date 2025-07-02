b9cce20566cdc04a8b06f922729e00f3161e2f16
Yamahn Tanjour <yamtanjour@gmail.com>
2025-07-02 22:49:29 +0200

    Added both screens, set up screen manager

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class HomeScreen(Screen):
+    pass
+
+class MyWorkoutsScreen(Screen):
+    pass
 
 class WorkItApp(App):
     def build(self):
-        Window.clearcolor = (0.12, 0.12, 0.12, 1)  # Even darker background
-        Window.size = (360, 640)  # Portrait aspect ratio for desktop testing
-
+        Window.clearcolor = (0.12, 0.12, 0.12, 1)
+        Window.size = (360, 640)
+        sm = ScreenManager()
+        sm.add_widget(HomeScreen(name='home'))  # <-- use 'home'
+        sm.add_widget(MyWorkoutsScreen(name='my_workouts'))
+        return sm
+    
 if __name__ == "__main__":
     WorkItApp().run()
